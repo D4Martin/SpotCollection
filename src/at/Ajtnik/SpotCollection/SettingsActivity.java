3545c272c7666705f2120b5000e8610b96262228
D4Martin <martin.ajtnik1@gmail.com>
2015-01-16 23:58:08 +0100

    Erstellung der Grunddatenklassen für das Projekt

---
+++ src/at/Ajtnik/SpotCollection/SettingsActivity.java
@@ -0,0 +1,15 @@
+package at.Ajtnik.SpotCollection;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ajtma_000 on 16.01.2015.
+ */
+public class SettingsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+}
