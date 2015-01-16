a56b85f0f52e3e479c56d7f37267b97c091a79c8
Philipp Schauzer <philipp.schauzer@gmx.at>
2015-01-16 22:55:41 +0100

    Prototype GUI

--- src/at/Ajtnik/SpotCollection/MyActivity.java
+++ src/at/Ajtnik/SpotCollection/MyActivity.java
@@ -1,15 +1,31 @@
 package at.Ajtnik.SpotCollection;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    ImageButton searchButton;
+    ImageButton menuButton;
+    EditText searchText;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        searchButton = (ImageButton)findViewById(R.id.btSearch);
+        menuButton  = (ImageButton)findViewById(R.id.btMenu);
+        searchText = (EditText)findViewById(R.id.tfSearchText);
+
+    }
+
+    public void OnMenu()
+    {
+        Intent intent = new Intent(getApplicationContext(),MenuActivity.class);
+        startActivity(intent);
     }
 }
