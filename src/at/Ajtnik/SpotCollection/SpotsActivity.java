e81eb2556576fca2a3debd898abf633c3248b831
Philipp Schauzer <philipp.schauzer@gmx.at>
2015-01-17 01:57:15 +0100

    Prototype GUI

---
+++ src/at/Ajtnik/SpotCollection/SpotsActivity.java
@@ -0,0 +1,15 @@
+package at.Ajtnik.SpotCollection;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Schauzer on 17.01.2015.
+ */
+public class SpotsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spots_activity);
+    }
+}
