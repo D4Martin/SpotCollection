0bcaf422997b0d046c29dcdd9ef38bcda2f0ae78
D4Martin <martin.ajtnik1@gmail.com>
2015-01-17 22:20:28 +0100

    Map Activity

--- src/at/Ajtnik/SpotCollection/MapActivity.java
+++ src/at/Ajtnik/SpotCollection/MapActivity.java
@@ -2,14 +2,21 @@ package at.Ajtnik.SpotCollection;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 /**
  * Created by ajtma_000 on 17.01.2015.
  */
-public class MapActivity extends Activity{
+public class MapActivity extends Activity implements OnMapReadyCallback{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
