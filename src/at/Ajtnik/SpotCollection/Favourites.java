866b6c16358ee1d42b215eb8d676d11279b26064
D4Martin <martin.ajtnik1@gmail.com>
2015-01-17 01:57:21 +0100

    SettingsActivity - Class update

---
+++ src/at/Ajtnik/SpotCollection/Favourites.java
@@ -0,0 +1,15 @@
+package at.Ajtnik.SpotCollection;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ajtma_000 on 17.01.2015.
+ */
+public class Favourites extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favourites);
+    }
+}
